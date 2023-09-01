ada4ce7f856da6c82b8e0c34d918e25c60db7eb6
Iamneo <techadmin@iamneo.ai>
2023-09-01 14:10:38 +0000

    Backup Commit

--- Cucumber/src/test/java/stepdefination/run.java
+++ Cucumber/src/test/java/stepdefination/run.java
@@ -4,4 +4,8 @@ import org.junit.runner.RunWith;
 import cucumber.junit.Cucumber; 
 @RunWith(Cucumber.class) 
 @Cucumber.Options(format= {"pretty", "html:target/cucumber"}) 
-public class run {}
+public class run {
+
+public static void main(String[] args){
+    System.out.println("Hello world");
+}}
